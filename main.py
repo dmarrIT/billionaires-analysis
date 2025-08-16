868cac837f31ec6463ec5f478c52ade2c564df9a
Dylan Marr <dylanmarr@me.com>
2025-08-16 15:55:09 -0500

    Added indexes, refactored variables, made first SQL query

--- main.py
+++ main.py
@@ -5,43 +5,72 @@ import sqlite3
 
 # ========== Read data =========
 
-data = pd.read_csv("./data/billionaires.csv")
-# print(data.shape, data.info(),'\n', data.head(10))
+raw_data = pd.read_csv("./data/billionaires.csv")
+# print(raw_data.shape, raw_data.info(),'\n', raw_data.head(10))
 
 # ========== Clean data ==========
 
-data.rename(columns={
+raw_data.rename(columns={
     "net_worth": "net_worth_billion_usd",
     "natinality": "nationality"
     }, inplace=True)
-# print(data.head(10))
+# print(raw_data.head(10))
 
 # standardize object data types
-for col in data.columns:
-    if data[col].dtype == "object":
-        data[col] = data[col].str.strip()
+for col in raw_data.columns:
+    if raw_data[col].dtype == "object":
+        raw_data[col] = raw_data[col].str.strip()
 
-# check for missing values
-print(data.isnull().sum())
+# # check for missing values
+# print(raw_data.isnull().sum())
 
 # ensure data type is correct for numeric values
-data["net_worth_billion_usd"] = pd.to_numeric(data["net_worth_billion_usd"], errors="coerce")
-data["age"] = pd.to_numeric(data["age"], errors="coerce")
-data["year"] = pd.to_numeric(data["year"], errors="coerce")
-data["rank"] = pd.to_numeric(data["rank"], errors="coerce")
+raw_data["net_worth_billion_usd"] = pd.to_numeric(raw_data["net_worth_billion_usd"], errors="coerce")
+raw_data["age"] = pd.to_numeric(raw_data["age"], errors="coerce")
+raw_data["year"] = pd.to_numeric(raw_data["year"], errors="coerce")
+raw_data["rank"] = pd.to_numeric(raw_data["rank"], errors="coerce")
 
 # export cleaned data
-cleaned_data = data
+cleaned_data = raw_data
 cleaned_data.to_csv("./exports/billionaires_cleaned.csv", index=False)
 
 # ========== Add cleaned data to db ==========
 
+# create db folder and connection
 pathlib.Path("./db").mkdir(parents=True, exist_ok=True)
 con = sqlite3.connect("./db/spotify.db")
+
+# load cleaned data to db
+cleaned_data.to_sql("billionaires", con, if_exists="replace", index=False)
+
+# add indexes for performance
+con.execute("CREATE INDEX IF NOT EXISTS idx_nationality ON billionaires(nationality)")
+con.execute("CREATE INDEX IF NOT EXISTS idx_networth ON billionaires(net_worth_billion_usd)")
+con.execute("CREATE INDEX IF NOT EXISTS idx_age ON billionaires(age)")
+con.execute("CREATE INDEX IF NOT EXISTS idx_source ON billionaires(source_wealth)")
+con.commit()
+
+# # create cursor, view database for testing
+# cur = con.cursor()
+# cur.execute("PRAGMA table_info(billionaires);")
+# schema = cur.fetchall()
+# for col in schema:
+#     print(col)
+
 
 # ========== Answer questions ==========
 
 # What is the percentage breakdown of billionaires by nationality in this data set?
+
+query = """
+SELECT nationality, SUM(net_worth_billion_usd) as total_billion_usd
+FROM billionaires
+GROUP BY nationality
+ORDER BY total_billion_usd DESC
+"""
+
+df_by_nationality = pd.read_sql_query(query, con)
+print(df_by_nationality)
 
 # What is the mean, median, and mode net worth in this data set?
 
@@ -49,7 +78,7 @@ con = sqlite3.connect("./db/spotify.db")
 
 # Which what are the top five sources of wealth by net worth in this data set?
 
-# What are the top three trending billionaires?
+# Who are the top three trending billionaires?
 
 # =========== Visualize insights ==========
 
